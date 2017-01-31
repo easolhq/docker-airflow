7db5874a2d63ae56af75eb91a072cbcf1c4d06f4
Taylor Edmiston <tedmiston@gmail.com>
2017-01-31 05:26:23 -0500

    Drop debug statements

--- airflow_home/plugins/operators/common.py
+++ airflow_home/plugins/operators/common.py
@@ -8,6 +8,3 @@ def config_s3():
     aws_key = os.getenv('AWS_ACCESS_KEY_ID', '')
     aws_secret = quote_plus(os.getenv('AWS_SECRET_ACCESS_KEY', ''))
     os.environ[CONN_ENV_PREFIX + 'S3_CONNECTION'] = 's3://{aws_key}:{aws_secret}@S3'.format(aws_key=aws_key, aws_secret=aws_secret)
-    print('RAN config_s3')
-
-print('LOADED common')

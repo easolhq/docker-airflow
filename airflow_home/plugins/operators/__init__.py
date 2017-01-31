e9a40ecfaf0b3ab3a70c1b0ea47847e5fdf69db8
Taylor Edmiston <tedmiston@gmail.com>
2017-01-31 05:27:02 -0500

    Update filename to reflect class name for S3WildcardKeySensor

--- airflow_home/plugins/operators/__init__.py
+++ airflow_home/plugins/operators/__init__.py
@@ -1,5 +1,3 @@
 from plugins.operators.s3_file_key_sensor import S3FileKeySensor
 from plugins.operators.s3_get_key_operator import S3GetKeyOperator
-from plugins.operators.s3_key_sensor import (
-    S3WildcardKeySensor,
-)
+from plugins.operators.s3_wildcard_key_sensor import S3WildcardKeySensor

22111b643442fefb402ef202e18a67da7494323e
Taylor Edmiston <tedmiston@gmail.com>
2017-01-31 05:25:00 -0500

    Simplify plugin operators imports

--- airflow_home/plugins/operators/__init__.py
+++ airflow_home/plugins/operators/__init__.py
@@ -1,5 +1,5 @@
-from plugins.operators.astronomer_s3_key_sensor import AstronomerS3GetKeyAction, AstronomerS3KeySensor, AstronomerS3WildcardKeySensor
-
-__all__ = [
-    'AstronomerS3KeySensor'
-]
+from plugins.operators.astronomer_s3_key_sensor import (
+    AstronomerS3GetKeyAction,
+    AstronomerS3KeySensor,
+    AstronomerS3WildcardKeySensor,
+)

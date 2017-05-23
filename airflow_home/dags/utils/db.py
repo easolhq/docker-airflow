7316ce1ee24340c040bff7819203c29b3c584627
Taylor Edmiston <tedmiston@gmail.com>
2017-05-23 16:08:58 -0400

    Refactor mongo pipeline creation into method

--- airflow_home/dags/utils/db.py
+++ airflow_home/dags/utils/db.py
@@ -3,73 +3,8 @@ import pymongo
 import ssl
 from bson.objectid import ObjectId
 
-# Build an aggregation pipeline that populates connections and vpnConnections
-pipeline = [
-    {
-        "$unwind": "$activityList"
-    },
-    {
-        "$lookup": {
-            "from": "connectionConfigs",
-            "localField": "activityList.connection",
-            "foreignField": "_id",
-            "as": "activityList.config.connection"
-        }
-    },
-    {
-        "$group": {
-            "_id": "$_id",
-            "name": {"$first": "$name"},
-            "path": {"$first": "$path"},
-            "schedule": {"$first": "$schedule"},
-            "pokeInterval": {"$first": "$pokeInterval"},
-            "timeout": {"$first": "$timeout"},
-            "accountId": {"$first": "$accountId"},
-            "activityList": {"$push": "$activityList"}
-        }
-    },
-    {
-        "$unwind": '$activityList'
-    },
-    {
-        "$unwind": {
-            "path": "$activityList.config.connection",
-            "preserveNullAndEmptyArrays": True
-        }
-    },
-    {
-        "$lookup": {
-            "from": "connectionConfigs",
-            "localField": "activityList.config.connection.vpnConnection",
-            "foreignField": "_id",
-            "as": "activityList.config.connection.vpnConnection"
-        }
-    },
-    {
-        "$unwind": '$activityList'
-    },
-    {
-        "$unwind": {
-            "path": "$activityList.config.connection.vpnConnection",
-            "preserveNullAndEmptyArrays": True
-        }
-    },
-    {
-        "$group": {
-            "_id": '$_id',
-            "name": {"$first": "$name"},
-            "path": {"$first": "$path"},
-            "schedule": {"$first": "$schedule"},
-            "pokeInterval": {"$first": "$pokeInterval"},
-            "timeout": {"$first": "$timeout"},
-            "accountId": {"$first": "$accountId"},
-            "activityList": {"$push": "$activityList"}
-        }
-    }
-]
-
-
 class MongoClient:
+
     def __init__(self):
         # Get mongo url.
         mongo_url = os.getenv('MONGO_URL', '')
@@ -78,15 +13,84 @@ class MongoClient:
         print('Connecting to mongodb.')
         self.client = pymongo.MongoClient(mongo_url, ssl_cert_reqs=ssl.CERT_NONE)
         self.db = self.client.get_default_database()
+        self.pipeline = self._build_pipeline()
+
+    def _build_pipeline(self):
+        """
+        Build an aggregation pipeline that populates connections and VPN connections.
+        """
+        return [
+            {
+                "$unwind": "$activityList"
+            },
+            {
+                "$lookup": {
+                    "from": "connectionConfigs",
+                    "localField": "activityList.connection",
+                    "foreignField": "_id",
+                    "as": "activityList.config.connection"
+                }
+            },
+            {
+                "$group": {
+                    "_id": "$_id",
+                    "name": {"$first": "$name"},
+                    "path": {"$first": "$path"},
+                    "schedule": {"$first": "$schedule"},
+                    "pokeInterval": {"$first": "$pokeInterval"},
+                    "timeout": {"$first": "$timeout"},
+                    "accountId": {"$first": "$accountId"},
+                    "activityList": {"$push": "$activityList"}
+                }
+            },
+            {
+                "$unwind": '$activityList'
+            },
+            {
+                "$unwind": {
+                    "path": "$activityList.config.connection",
+                    "preserveNullAndEmptyArrays": True
+                }
+            },
+            {
+                "$lookup": {
+                    "from": "connectionConfigs",
+                    "localField": "activityList.config.connection.vpnConnection",
+                    "foreignField": "_id",
+                    "as": "activityList.config.connection.vpnConnection"
+                }
+            },
+            {
+                "$unwind": '$activityList'
+            },
+            {
+                "$unwind": {
+                    "path": "$activityList.config.connection.vpnConnection",
+                    "preserveNullAndEmptyArrays": True
+                }
+            },
+            {
+                "$group": {
+                    "_id": '$_id',
+                    "name": {"$first": "$name"},
+                    "path": {"$first": "$path"},
+                    "schedule": {"$first": "$schedule"},
+                    "pokeInterval": {"$first": "$pokeInterval"},
+                    "timeout": {"$first": "$timeout"},
+                    "accountId": {"$first": "$accountId"},
+                    "activityList": {"$push": "$activityList"}
+                }
+            }
+        ]
 
     def workflow_configs(self):
-        return self.db.workflows.aggregate(pipeline)
+        return self.db.workflows.aggregate(self.pipeline)
 
     def webhook_configs(self):
-        return self.db.webhookConfigs.aggregate(pipeline)
+        return self.db.webhookConfigs.aggregate(self.pipeline)
 
     def ftp_configs(self):
-        return self.db.ftpConfigs.aggregate(pipeline)
+        return self.db.ftpConfigs.aggregate(self.pipeline)
 
     def clickstream_configs(self):
         # TODO: explain

a538afd7e2e690d548e347456c60f4971f45f7d6
Taylor Edmiston <tedmiston@gmail.com>
2017-05-24 11:22:05 -0400

    Simplify clickstream sensor

--- airflow_home/plugins/operators/s3_clickstream_key_sensor.py
+++ airflow_home/plugins/operators/s3_clickstream_key_sensor.py
@@ -1,5 +1,5 @@
 """
-TODO
+Clickstream events file detector.
 """
 
 import logging
@@ -14,6 +14,11 @@ from plugins.operators.common import config_s3
 config_s3()
 
 
+def build_s3_wildcard_url(bucket, key):
+    """Create S3 URL for wildcard path."""
+    return 's3://{bucket}/{key}*'.format(bucket=bucket, key=key)
+
+
 class S3ClickstreamKeySensor(BaseSensorOperator):
     """Detect an execution-date bound file path in S3."""
 
@@ -21,34 +26,27 @@ class S3ClickstreamKeySensor(BaseSensorOperator):
 
     @apply_defaults
     def __init__(self, bucket_key, bucket_name, timedelta=0, *args, **kwargs):
-        """
-        Initialize sensor.
-        """
+        """Initialize sensor."""
         super(S3ClickstreamKeySensor, self).__init__(*args, **kwargs)
         self.bucket_name = bucket_name
         self.bucket_key = bucket_key
         self.timedelta = timedelta
 
+    def _build_url(self, context):
+        task_instance = context['ti']
+        # TODO: does the datetime part here need to change since we're shifting the whole DAG back on delay?
+        batch_datetime = task_instance.execution_date - timedelta(minutes=((execution_date.minute % 15) + self.timedelta))
+        bucket_key = self.bucket_key.format(date=batch_datetime.strftime("%Y-%m-%dT%H_%M_%S"))
+        full_url = build_s3_wildcard_url(bucket=self.bucket_name, key=bucket_key)
+        logging.info('Poking for key "{}"'.format(full_url))
+        return full_url
+
     def poke(self, context):
         """Poke for clickstream event files."""
         logging.info('Starting poke')
         hook = S3FileHook(s3_conn_id='S3_CONNECTION')
-        # TODO: where does this context['ti'] get populated from?  is this task ID?
-        execution_date = context['ti'].execution_date
-        # TODO: does the datetime part here need to change since we're shifting the whole DAG back on delay?
-        batch_datetime = execution_date - timedelta(
-            minutes=((execution_date.minute % 15) + self.timedelta)
-        )
-        bucket_key = self.bucket_key.format(
-            date=batch_datetime.strftime("%Y-%m-%dT%H_%M_%S")
-        )
-        full_url = 's3://' + self.bucket_name + '/' + bucket_key + '*'
-        logging.info('Poking for key "{}"'.format(full_url))
-
-        file_found = hook.check_for_wildcard_key(
-            wildcard_key=full_url,
-            delimiter='/',
-        )
+        full_url = self._build_url(context=context)
+        file_found = hook.check_for_wildcard_key(wildcard_key=full_url, delimiter='/')
 
         if not file_found:
             logging.warning('No file found at "{}"'.format(full_url))

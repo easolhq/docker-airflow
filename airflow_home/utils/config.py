68c4f722e1fc8b6889985d317a07c388f60b5cfd
Taylor Edmiston <tedmiston@gmail.com>
2017-05-31 16:34:48 -0400

    Add logging for missing param names

--- airflow_home/utils/config.py
+++ airflow_home/utils/config.py
@@ -2,6 +2,11 @@
 Clickstream config.
 """
 
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 class ClickstreamActivity(object):
     """Config for clickstream event classes."""
@@ -20,19 +25,21 @@ class ClickstreamActivity(object):
         self.temp_bucket = temp_bucket
         self.name, self.version = name_ver.split(':', 1) if name_ver is not None else 'aries-activity-aries-base', '0.1'
 
-        self.required_params = [
-            self.workflow_id,
-            self.table_name,
-            self.redshift_host,
-            self.redshift_port,
-            self.redshift_db,
-            self.redshift_user,
-            self.redshift_password,
-            self.redshift_encrypted,
-            self.temp_bucket,
-            self.name,
-            self.version
+        required_params_strs = [
+            'workflow_id',
+            'table_name',
+            'redshift_host',
+            'redshift_port',
+            'redshift_db',
+            'redshift_user',
+            'redshift_password',
+            'redshift_encrypted',
+            'temp_bucket',
+            'name',
+            'version',
         ]
+        required_params_vals = [getattr(self, i) for i in required_params_strs]
+        self.required_params = dict(zip(required_params_strs, required_params_vals))
 
     @property
     def redshift_schema(self):
@@ -45,7 +52,11 @@ class ClickstreamActivity(object):
 
     def is_valid(self):
         # TODO: handle if values are empty strings
-        missing_params = [i for i in self.required_params if i is None]
+        missing_params = []
+        for name, value in self.required_params.items():
+            if value is None:
+                logger.info('Clickstream param {} is missing'.format(name))
+                missing_params.append(value)
         return len(missing_params) == 0
 
     def serialize(self):

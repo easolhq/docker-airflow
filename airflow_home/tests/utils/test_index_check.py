f8fbd94520ae52f25bf48c28a41414e04ee9426a
trong-le <trong.p.le.92@gmail.com>
2017-06-16 11:50:53 -0400

    added index_check tests, checked for no depends_on, some, and none. refactored index_check to return None if no match

--- airflow_home/tests/utils/test_index_check.py
+++ airflow_home/tests/utils/test_index_check.py
@@ -1,8 +1,20 @@
 from utils.index_check import index_check
 
 
-def test_index_check():
-    depends_on = [1]
+def test_single_index_check():
     activity_list = [{"name": "test_1", "id": 1}, {"name": "test_2", "id": 2, "depends_on": [1]}]
     index = index_check(1, activity_list)
     assert index == 0
+
+
+def test_no_index_check():
+    activity_list = [{"name": "test_1", "id": 1}, {"name": "test_2", "id": 2}]
+    index = index_check(0, activity_list)
+    assert index is None
+
+
+def test_multiple_index_check():
+    activity_list = [{"name": "test_1", "id": 1}, {"name": "test_2", "id": 2, "depends_on": [1]},
+                     {"name": "test_3", "id": 3, "depends_on": [2]}]
+    index = index_check(2, activity_list)
+    assert index == 1
--- airflow_home/utils/index_check.py
+++ airflow_home/utils/index_check.py
@@ -1,2 +1,9 @@
 def index_check(value, activity_list):
-    return next(index for (index, activity) in enumerate(activity_list) if activity["id"] == value)
+    """
+    Returns index of activity based on id
+    :param value: the id of to search for the index
+    :type value: integer
+    :param activity_list: the list of activities
+    :type activity_list: list
+    """
+    return next((index for (index, activity) in enumerate(activity_list) if activity["id"] == value), None)

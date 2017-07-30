53f8aec1ee05458ed5c8d11eb2c3c5fc8f65df85
Tyler Michael <tmich214@gmail.com>
2017-07-29 22:24:10 -0400

    change dag_id for clickstream dags

--- airflow_home/utils/redshift.py
+++ airflow_home/utils/redshift.py
@@ -10,7 +10,6 @@ def build_dag_id(workflow):
     TODO
     """
     workflow_id = workflow['_id']
-    workflow_name = workflow.get('name', 'astronomer_clickstream_to_redshift')
-    workflow_name = boa.constrict(workflow_name.lower())
-    dag_id = '{name}__etl__{id}'.format(id=workflow_id, name=workflow_name)
+    workflow_name = 'clickstream_to_redshift'
+    dag_id = '{name}__{id}'.format(id=workflow_id, name=workflow_name)
     return dag_id

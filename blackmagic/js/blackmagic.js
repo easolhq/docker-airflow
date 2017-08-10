59633f80dcd4f5a9f9b467a965982f217939825d
Taylor Edmiston <tedmiston@gmail.com>
2017-08-10 04:34:34 -0400

    Fail fast when stdout stream has closed unexpectedly

--- blackmagic/js/blackmagic.js
+++ blackmagic/js/blackmagic.js
@@ -8,6 +8,7 @@ const cryptobject = require('cryptobject');
  */
 function lookupCommandFunc(name) {
   // func = the js function to run
+  let func;
   if (name === 'encrypt') {
     func = cryptobject.encrypt;
   } else if (name === 'decrypt') {
@@ -21,14 +22,25 @@ function lookupCommandFunc(name) {
 
 /**
  * Apply the desired encryption function, JSONifying from stdin and to stdout.
+ *
+ * For some reason (see [1]), this write to stdout sometimes fails because the
+ * stdout stream has randomly closed [2][3] which breaks the upstream return to
+ * Python.
+ *
+ * 1: https://github.com/aries-data/aries-data/blob/797ea69b1172de1feefd95a07dd4b57c54df9fa8/lib/cli/aries-data.js#L15
+ * 2: https://github.com/nodejs/node/issues/947
+ * 3: https://github.com/nodejs/node-v0.x-archive/issues/3211
  */
 function apply() {
-  func = lookupCommandFunc(argv.command);
-  process.stdout.write(
-    JSON.stringify(
-      func(argv.passphrase, JSON.parse(argv.obj))
-    )
-  );
+  const func = lookupCommandFunc(argv.command);
+  const rv = func(argv.passphrase, JSON.parse(argv.obj));
+  try {
+    console.log(JSON.stringify(rv));
+    setTimeout(process.exit.bind(process, 0), 1000);
+  } catch (e) {
+    console.log(JSON.stringify({}));
+    setTimeout(process.exit.bind(process, 1), 1000);
+  }
 }
 
 apply();
